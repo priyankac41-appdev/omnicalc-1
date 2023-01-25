be1d801e64e805b4c70ce1c2f1002c4f06527320
priyankac41 <pchandr1@chicagobooth.edu>
2023-01-25 05:03:49 +0000

    square defs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
 
+  # get("", {:controller => "", :action => ""})
+  # this controller is pre-made from rails and is out of the box and comes loaded as applicaion_controller.rb
+  # job is to show the user the form when they want to do the square
+  get("/square/new", {:controller => "application", :action => "blank_square_form"})
+  get("/square/results", {:controller => "application", :action => "calculate_square"})
+  # get("", {:controller => "", :action => ""})
+
 end
