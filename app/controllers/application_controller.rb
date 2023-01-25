1f6326058af4de45f5097adb620045d210243cfd
priyankac41 <pchandr1@chicagobooth.edu>
2023-01-25 05:17:31 +0000

    square root done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,14 @@ def calculate_square
   render({:template => "calculation_templates/square_results.html.erb"})
 end
 
+def blank_square_root_form
+  render({:template => "calculation_templates/square_root_form.html.erb"})
+end
+
+def calculate_square_root
+  @num = params.fetch("value_to_square_root").to_f
+  @sqrt_of_num = Math.sqrt(@num)
+  render({:template => "calculation_templates/square_root_results.html.erb"})
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # job is to show the user the form when they want to do the square
   get("/square/new", {:controller => "application", :action => "blank_square_form"})
   get("/square/results", {:controller => "application", :action => "calculate_square"})
-  # get("", {:controller => "", :action => ""})
+  get("/square_root/new", {:controller => "application", :action => "blank_square_root_form"})
+  get("/square_root/results", {:controller => "application", :action => "calculate_square_root"})
 
 end
