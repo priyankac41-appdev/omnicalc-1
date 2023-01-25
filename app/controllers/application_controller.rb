83285ac99fc683413a82cef4017a88276bf5a86f
priyankac41 <pchandr1@chicagobooth.edu>
2023-01-25 05:56:44 +0000

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,30 @@ def calculate_square_root
   render({:template => "calculation_templates/square_root_results.html.erb"})
 end
 
+def blank_payment_form
+  render({:template => "calculation_templates/payment_form.html.erb"})
+end
+
+def calculate_payment
+  @apr = params.fetch("apr").to_f.round(4)
+  @years = params.fetch("years").to_f
+  @principal = params.fetch("principal").to_f
+  payment_num = @apr/100/12 * @principal
+  payment_denom = 1 - (1+@apr/100/12)**(-@years*12)
+  @payment = payment_num / payment_denom
+  render({:template => "calculation_templates/payment_form_results.html.erb"})
+end
+
+def blank_random_form
+  render({:template => "calculation_templates/random_form.html.erb"})
+end
+
+def calculate_random
+  @min = params.fetch("min").to_f
+  @max = params.fetch("max").to_f
+  @random_value = rand(@min..@max)
+  render({:template => "calculation_templates/random_results.html.erb"})
+end
+
+
 end
